b9a3d95c4ca06a3b0cf25be95ec6766a4fdae811
Kubster96 <jungiewiczjakub@gmail.com>
2018-01-23 13:32:10 +0100

    add Planet class

--- Main.py
+++ Main.py
@@ -1,68 +1,83 @@
 from vpython import *
 
-# constants
-R1 = 1
-R2 = 1
-R3 = 1
-M1 = 1
-M2 = 1
-M3 = 1 / 10
+from Planet import Planet
+
 G = 100
 
-# creating the stars
-planet1 = sphere(pos=vector(4.5, 0, 0), radius=R1, color=color.yellow)
-planet2 = sphere(pos=vector(-4.5, 0, 0), radius=R2, color=color.orange)
-planet3 = sphere(pos=vector(-10, 0, 0), radius=R3, color=color.cyan)
+# just for init Planet(radius, mass, position, momentum)
+# planet1 = Planet(1, 1, (4.5, 0, 0), (0, 2, 0))
+# planet2 = Planet(1, 1, (-4.5, 0, 0), (0, -3, 0))
+# planet3 = Planet(1, 0.1, (-10, 0, 0), (0, 0.1, 0))
 
-planet1.m = M1
-planet2.m = M2
-planet3.m = M3
+# planet1 = Planet(2, 1, (5.5, 0, 0), (0, 2, 0))
+# planet2 = Planet(1, 1, (-4.5, 0, 0), (0, -3, 0))
+# planet3 = Planet(3, 5, (-30, 0, 0), (0, 0.01, 0))
 
-planet1.p = vector(0, 2, 0)
-planet2.p = vector(0, -3, 0)
-planet3.p = vector(0, 0.1, 0)
+# one big inside two small orbiting
+# planet1 = Planet(1, 1/10, (20, 0, 0), (0, 1, 0))
+# planet2 = Planet(1, 2/10, (-10, 0, 0), (0, -3, 0))
+# planet3 = Planet(4, 20, (0, 0, 0), (0, 0, 0))
 
-attach_trail(planet1)
-attach_trail(planet2)
-attach_trail(planet3)
+# two stars
+planet1 = Planet(1, 1/10, (40, 0, 0), (0, 0.9, 0))
+planet2 = Planet(4, 10, (20, 0, 0), (0, -20, 0))
+planet3 = Planet(4, 10, (-20, 0, 0), (0, 20, 0))
+
+
+# creating orbs
+orb1 = sphere(pos=vector(planet1.position_x, planet1.position_y, planet1.position_z), radius=planet1.radius, color=color.yellow)
+orb2 = sphere(pos=vector(planet2.position_x, planet2.position_y, planet2.position_z), radius=planet2.radius, color=color.orange)
+orb3 = sphere(pos=vector(planet3.position_x, planet3.position_y, planet3.position_z), radius=planet3.radius, color=color.cyan)
+
+orb1.m = planet1.mass
+orb2.m = planet2.mass
+orb3.m = planet3.mass
+
+orb1.p = vector(planet1.momentum_x, planet1.momentum_y, planet1.momentum_z)
+orb2.p = vector(planet2.momentum_x, planet2.momentum_y, planet2.momentum_z)
+orb3.p = vector(planet3.momentum_x, planet3.momentum_y, planet3.momentum_z)
+
+attach_trail(orb1)
+attach_trail(orb2)
+attach_trail(orb3)
 
 
 t = 0
 dt = 0.0005
 
-while t < 150:
+while t < 1000:
     rate(10000)
 
     # vector from star 1 to 2
-    vec12 = planet2.pos - planet1.pos
+    vec12 = orb2.pos - orb1.pos
     vec21 = -vec12
 
     # vector from star 1 to planet
-    vec13 = planet3.pos - planet1.pos
+    vec13 = orb3.pos - orb1.pos
     vec31 = -vec13
 
     # vector from star 2 to planet
-    vec23 = planet3.pos - planet2.pos
+    vec23 = orb3.pos - orb2.pos
     vec32 = -vec23
 
     # calculate grav force on star 1 due to 2
-    F12 = -G * planet1.m * planet2.m * norm(vec12) / mag(vec12) ** 2
-    F21 = -G * planet1.m * planet2.m * norm(vec21) / mag(vec21) ** 2
+    F12 = -G * orb1.m * orb2.m * norm(vec12) / mag(vec12) ** 2
+    F21 = -G * orb1.m * orb2.m * norm(vec21) / mag(vec21) ** 2
     # calculate the force on planet due to star 1
-    F13 = -G * planet1.m * planet3.m * norm(vec13) / mag(vec13) ** 2
-    F31 = -G * planet1.m * planet3.m * norm(vec31) / mag(vec31) ** 2
+    F13 = -G * orb1.m * orb3.m * norm(vec13) / mag(vec13) ** 2
+    F31 = -G * orb1.m * orb3.m * norm(vec31) / mag(vec31) ** 2
     # calculate the force on planet due to star 2
-    F23 = -G * planet2.m * planet3.m * norm(vec23) / mag(vec23) ** 2
-    F32 = -G * planet2.m * planet3.m * norm(vec32) / mag(vec32) ** 2
+    F23 = -G * orb2.m * orb3.m * norm(vec23) / mag(vec23) ** 2
+    F32 = -G * orb2.m * orb3.m * norm(vec32) / mag(vec32) ** 2
 
     # update momentum (with total vector force)
-    planet1.p = planet1.p + (F21 + F31) * dt
-    planet2.p = planet2.p + (F12 + F32) * dt
-    planet3.p = planet3.p + (F13 + F23) * dt
+    orb1.p = orb1.p + (F21 + F31) * dt
+    orb2.p = orb2.p + (F12 + F32) * dt
+    orb3.p = orb3.p + (F13 + F23) * dt
 
     # update position
-    planet1.pos = planet1.pos + planet1.p * dt / planet1.m
-    planet2.pos = planet2.pos + planet2.p * dt / planet2.m
-    planet3.pos = planet3.pos + planet3.p * dt / planet3.m
+    orb1.pos = orb1.pos + orb1.p * dt / orb1.m
+    orb2.pos = orb2.pos + orb2.p * dt / orb2.m
+    orb3.pos = orb3.pos + orb3.p * dt / orb3.m
 
     t = t + dt
---
+++ Planet.py
@@ -0,0 +1,25 @@
+
+class Planet:
+
+    radius = 0
+
+    mass = 0
+
+    position_x = 0
+    position_y = 0
+    position_z = 0
+
+    momentum_x = 0
+    momentum_y = 0
+    momentum_z = 0
+
+    def __init__(self, r, m, position, momentum):
+
+        self.radius = r
+        self.mass = m
+        self.position_x = position[0]
+        self.position_y = position[1]
+        self.position_z = position[2]
+        self.momentum_x = momentum[0]
+        self.momentum_y = momentum[1]
+        self.momentum_z = momentum[2]
